b21d50d3f7e4833836b4a5228c42a263cd493f86
Antonin Décimo <antonin@tarides.com>
2023-12-19 18:15:39 +0100

    winpthreads: tests: Update struct sizes report
    - mutex_t has become the private mutex_impl_t type.
    
    Signed-off-by: Antonin Décimo <antonin@tarides.com>

--- mingw-w64-libraries/winpthreads/tests_pthread/sizes.c
+++ mingw-w64-libraries/winpthreads/tests_pthread/sizes.c
@@ -9,18 +9,18 @@ main(void)
   printf("Sizes of winpthreads structs\n");
   printf("-------------------------------\n");
   printf("%30s %4" PRIdPTR "\n", "pthread_t", sizeof(pthread_t));
-  printf("%30s %4" PRIdPTR "\n", "pthread_attr_t_", sizeof(pthread_attr_t));
-  /*  printf("%30s %4" PRIdPTR "\n", "sem_t_", sizeof(struct sem_t_)); */
-  printf("%30s %4" PRIdPTR "\n", "pthread_mutex_t_", sizeof(mutex_t));
+  printf("%30s %4" PRIdPTR "\n", "pthread_attr_t", sizeof(pthread_attr_t));
+  /*  printf("%30s %4" PRIdPTR "\n", "sem_t", sizeof(struct sem_t)); */
+  /* printf("%30s %4" PRIdPTR "\n", "mutex_t", sizeof(mutex_t)); */
   printf("%30s %4" PRIdPTR "\n", "pthread_mutexattr_t_", sizeof(pthread_mutexattr_t));
   printf("%30s %4" PRIdPTR "\n", "pthread_barrier_t_", sizeof(barrier_t));
-  printf("%30s %4" PRIdPTR "\n", "pthread_barrierattr_t_", sizeof(pthread_barrierattr_t));
-  printf("%30s %4" PRIdPTR "\n", "pthread_key_t_", sizeof(pthread_key_t));
-  printf("%30s %4" PRIdPTR "\n", "pthread_cond_t_", sizeof(cond_t));
-  printf("%30s %4" PRIdPTR "\n", "pthread_condattr_t_", sizeof(pthread_condattr_t));
-  printf("%30s %4" PRIdPTR "\n", "pthread_rwlock_t_", sizeof(rwlock_t));
-  printf("%30s %4" PRIdPTR "\n", "pthread_rwlockattr_t_", sizeof(pthread_rwlockattr_t));
-  printf("%30s %4" PRIdPTR "\n", "pthread_once_t_", sizeof(pthread_once_t));
+  printf("%30s %4" PRIdPTR "\n", "pthread_barrierattr_t", sizeof(pthread_barrierattr_t));
+  printf("%30s %4" PRIdPTR "\n", "pthread_key_t", sizeof(pthread_key_t));
+  printf("%30s %4" PRIdPTR "\n", "cond_t", sizeof(cond_t));
+  printf("%30s %4" PRIdPTR "\n", "pthread_condattr_t", sizeof(pthread_condattr_t));
+  printf("%30s %4" PRIdPTR "\n", "rwlock_t", sizeof(rwlock_t));
+  printf("%30s %4" PRIdPTR "\n", "pthread_rwlockattr_t", sizeof(pthread_rwlockattr_t));
+  printf("%30s %4" PRIdPTR "\n", "pthread_once_t", sizeof(pthread_once_t));
   /* printf("%30s %4" PRIdPTR "\n", "ptw32_cleanup_t", sizeof(struct ptw32_cleanup_t)); */
   /* printf("%30s %4" PRIdPTR "\n", "ptw32_mcs_node_t_", sizeof(struct ptw32_mcs_node_t_)); */
   printf("%30s %4" PRIdPTR "\n", "sched_param", sizeof(struct sched_param));
