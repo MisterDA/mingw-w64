76c6771bffbf40c473dad84878931d8e98dc7bee
Antonin Décimo <antonin@tarides.com>
2024-01-12 18:12:01 +0100

    wip

--- mingw-w64-libraries/winpthreads/src/nanosleep.c
+++ mingw-w64-libraries/winpthreads/src/nanosleep.c
@@ -9,13 +9,13 @@
 #include <windows.h>
 #include "pthread.h"
 #include "pthread_time.h"
-#include "winpthread_internal.h"
 
-#define POW10_3                 1000
-#define POW10_4                 10000
-#define POW10_6                 1000000
-#define POW10_9                 1000000000
-#define MAX_SLEEP_IN_MS         4294967294UL
+#define POW10_2 100LL
+#define POW10_3 1000LL
+#define POW10_4 10000LL
+#define POW10_6 1000000LL
+#define POW10_7 10000000LL
+#define POW10_9 1000000000LL
 
 /**
  * Sleep for the specified time.
@@ -27,47 +27,55 @@
  */
 int nanosleep(const struct timespec *request, struct timespec *remain)
 {
-    int rc = 0;
-    unsigned long ms;
-    unsigned __int64 u64, want, real;
-
-    union {
-        unsigned __int64 ns100;
-        FILETIME ft;
-    }  _start = { 0 }, _end;
+    const struct _pthread_v *s = __pthread_self_lite();
+    long long starting_time, ending_time, frequency;
+    HANDLE h;
+    DWORD rc;
 
     if (request->tv_sec < 0 || request->tv_nsec < 0 || request->tv_nsec >= POW10_9) {
         errno = EINVAL;
         return -1;
     }
 
-    if (remain != NULL) GetSystemTimeAsFileTime(&_start.ft);
+    QueryPerformanceCounter(&starting_time);
+    QueryPerformanceFrequency(&frequency);
 
-    want = u64 = (unsigned __int64) request->tv_sec * POW10_3
-               + (unsigned __int64) request->tv_nsec / POW10_6;
-    while (u64 > 0 && rc == 0) {
-        if (u64 >= MAX_SLEEP_IN_MS) ms = MAX_SLEEP_IN_MS;
-        else ms = (unsigned long) u64;
-
-        u64 -= ms;
-        rc = pthread_delay_np_ms(ms);
+    if (s->high_res_timer) {
+        /* 100ns units, relative time (negative) */
+        const long long dt = -(request->tv_sec * POW10_7 + request->tv_nsec / POW10_2);
+        h = s->high_res_timer;
+        SetWaitableTimer(h, &dt, 0, NULL, NULL, FALSE);
+        timeout = INFINITE;
+    } else {
+        const long long dt = request->tv_sec * POW10_3 + request->tv_nsec / POW10_6;
+        h = INVALID_HANDLE_VALUE;
+        timeout = dt < INFINITE ? dt : INFINITE - 1;
     }
 
-    if (rc != 0) { /* WAIT_IO_COMPLETION (192) */
-        if (remain != NULL) {
-            GetSystemTimeAsFileTime(&_end.ft);
-            real = (_end.ns100 - _start.ns100) / POW10_4;
+    pthread_testcancel();
+    rc = WaitForSingleObjectEx(h, timeout, TRUE);
+    pthread_testcancel();
 
-            if (real >= want) u64 = 0;
-            else u64 = want - real;
+    if (remain != NULL) {
+        long long ending_time, elapsed, frequency;
+        QueryPerformanceCounter(&ending_time);
+        QueryPerformanceFrequency(&frequency);
+        elapsed = (ending_time - starting_time) * POW10_9 / frequency;
+        remain->tv_sec = elapsed / POW10_9;
+        remain->tv_nsec = elapsed % POW10_9;
+    }
 
-            remain->tv_sec = u64 / POW10_3;
-            remain->tv_nsec = (long) (u64 % POW10_3) * POW10_6;
-        }
-
+    switch (rc) {
+    case WAIT_TIMEOUT:
+        return 0;
+    case WAIT_IO_COMPLETION:
+    case WAIT_OBJECT_0:
         errno = EINTR;
         return -1;
+    case WAIT_FAILED:
+        errno = 0;
+        return -1;
+    case WAIT_ABANDONED:
+        UNREACHABLE();
     }
-
-    return 0;
 }
